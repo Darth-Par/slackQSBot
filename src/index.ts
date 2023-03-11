ff82c3f00da4dd9edb5ee6eb9a230704cfff06b6
Darth-Par <pmicoo@hotmail.com>
2023-03-11 17:10:15 -0500

    remove build dir and cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,17 @@
 import https = require('https');
 
 const data = JSON.stringify({ text: 'Hello from GitHub' });
-// const data = 'Hello from GitHub';
-const slackWebHook =
-  'https://hooks.slack.com/services/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';
-
-// nodeFetch(slackWebHook, {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify(data),
-// })
-//   .then((response) => response.json())
-//   .then((result) => console.log(result))
-//   .catch((error) => {
-//     console.error('Error: ', error);
-//   });
-
 const options = {
+  hostname: 'hooks.slack.com',
+  port: '443',
+  path: '/services/TE5Q5HXUZ/B04TEH0V5V3/GaxPd2DtQUrmQ8tduK48FpQO',
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
   },
 };
 
-const req = https.request(slackWebHook, options, (res) => {
+const req = https.request(options, (res) => {
   console.log(`STATUS: ${res.statusCode}`);
   console.log(`MESSAGE: ${res.statusMessage}`);
   console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
