f900c15597b73a30820e7e44dc6b8bf357288c03
Darth-Par <pmicoo@hotmail.com>
2023-03-12 13:52:38 -0400

    no returns

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import * as actionsCore from '@actions/core';
+import * as core from '@actions/core';
 
 type SlackMessageBody = {
   text: string;
 };
 
-const slackUrl = actionsCore.getInput('slackWebHook');
-const inputMessage = actionsCore.getInput('message');
+const slackUrl = core.getInput('slackWebHook');
+const inputMessage = core.getInput('message');
 const data: SlackMessageBody = { text: inputMessage };
 
 const sendToSlack = async (url: string, messageBody: SlackMessageBody) => {
@@ -14,12 +14,10 @@ const sendToSlack = async (url: string, messageBody: SlackMessageBody) => {
     const response = await axios.post(url, JSON.stringify(messageBody), {
       headers: { 'Content-Type': 'application/json' },
     });
-    actionsCore.setOutput('response', JSON.stringify(response));
-    return 0;
+    core.setOutput('response', JSON.stringify(response.statusText));
   } catch (error) {
     const slackError = JSON.stringify(error);
-    actionsCore.setFailed(slackError);
-    return 1;
+    core.setFailed(slackError);
   }
 };
 
