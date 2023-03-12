b8c401db2e3e45d4158d97fbcaf7e0531b8924f6
Darth-Par <pmicoo@hotmail.com>
2023-03-12 17:58:07 -0400

    remove excess logging

--- src/index.ts
+++ src/index.ts
@@ -32,8 +32,6 @@ const sendToSlack = async (url: string, messageBody: SlackMessageBody) => {
       headers: { 'Content-Type': 'application/json' },
     });
     core.setOutput('response', JSON.stringify(response.statusText));
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`Payload: ${payload}`);
   } catch (error) {
     const slackError = JSON.stringify(error);
     core.setFailed(slackError);
