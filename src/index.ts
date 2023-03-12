ed164ca67acef7ee7e64812edd447eb35f7787b2
Darth-Par <pmicoo@hotmail.com>
2023-03-12 17:17:16 -0400

    using octokit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import { PushEvent } from '@octokit/webhooks-definitions/schema';
 
 type SlackMessageBody = {
   text: string;
@@ -8,16 +9,30 @@ type SlackMessageBody = {
 
 const slackUrl = core.getInput('slackWebHook');
 const inputMessage = core.getInput('message');
-const data: SlackMessageBody = { text: inputMessage };
+
+const getCommitData = async () => {
+  if (github.context.eventName == 'push') {
+    const pushPayload = github.context.payload as PushEvent;
+    const commitId = pushPayload.head_commit?.id;
+    const commitUrl = pushPayload.head_commit?.url;
+
+    return {
+      id: commitId,
+      url: commitUrl,
+    };
+  }
+
+  return undefined;
+};
 
 const sendToSlack = async (url: string, messageBody: SlackMessageBody) => {
   try {
     const response = await axios.post(url, JSON.stringify(messageBody), {
       headers: { 'Content-Type': 'application/json' },
     });
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
     core.setOutput('response', JSON.stringify(response.statusText));
+    const payload = github.context.payload;
+    console.log(`Payload: ${payload}`);
   } catch (error) {
     const slackError = JSON.stringify(error);
     core.setFailed(slackError);
@@ -25,5 +40,9 @@ const sendToSlack = async (url: string, messageBody: SlackMessageBody) => {
 };
 
 (async () => {
-  await sendToSlack(slackUrl, data);
+  const commitData = await getCommitData();
+  if (commitData) {
+    const messageBody = `Status: ${inputMessage}\nCommitId: ${commitData.id}\nCommitUrl: ${commitData.url}`;
+    await sendToSlack(slackUrl, { text: messageBody });
+  }
 })();
