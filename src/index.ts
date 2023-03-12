b840845cba59418411ef01ecf77246fd02409a88
Darth-Par <pmicoo@hotmail.com>
2023-03-12 16:18:08 -0400

    using watch mode, and checking github info

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 type SlackMessageBody = {
   text: string;
@@ -14,6 +15,8 @@ const sendToSlack = async (url: string, messageBody: SlackMessageBody) => {
     const response = await axios.post(url, JSON.stringify(messageBody), {
       headers: { 'Content-Type': 'application/json' },
     });
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
     core.setOutput('response', JSON.stringify(response.statusText));
   } catch (error) {
     const slackError = JSON.stringify(error);
