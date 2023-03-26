5877635acf736d5aacad703ca153831e71280afa
Darth-Par <pmicoo@hotmail.com>
2023-03-26 12:41:35 -0400

    Refactor for testing, added tests for 'getCommitData' function

---
+++ tests/getCommitData.test.ts
@@ -0,0 +1,38 @@
+import { describe, expect, test } from '@jest/globals';
+import { PushEvent } from '@octokit/webhooks-definitions/schema';
+
+import { getCommitData } from '../src';
+
+describe('getCommitData test suite', () => {
+  test('expect standard success return', async () => {
+    const ghEventName = 'push';
+    const ghPayload = {
+      head_commit: {
+        id: 'qerel3902345',
+        url: 'https://gh.com/someFakeThing',
+      },
+      repository: {
+        name: 'ghActionsRepo',
+      },
+    } as PushEvent;
+
+    const commitData = await getCommitData(ghEventName, ghPayload);
+    expect(commitData?.name).toBe('ghActionsRepo');
+  });
+
+  test('expect undefined return', async () => {
+    const ghEventName = 'PR';
+    const ghPayload = {
+      head_commit: {
+        id: 'qerel3902345',
+        url: 'https://gh.com/someFakeThing',
+      },
+      repository: {
+        name: 'ghActionsRepo',
+      },
+    } as PushEvent;
+
+    const commitData = await getCommitData(ghEventName, ghPayload);
+    expect(commitData).toBe(undefined);
+  });
+});
